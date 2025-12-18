21cea50f8560488b39a7e8b91e7912107d5c28fb
aadhyagupta20-del <aadhyagupta20@gmail.com>
2025-12-18 10:35:08 +0400

    typecasting.py

---
+++ typecasing.py
@@ -0,0 +1,14 @@
+name="Aadhya"
+age=12
+is_student=True
+Height=156.3
+print("The data types before typecasting are")
+print(is_student,type(is_student))
+print(name,type(name))
+print(age,type(age))
+print(Height,type(Height))
+print("The data types after typecasting are")
+age=float(age)
+print(age,type(age))
+Height=int(Height)
+print(Height,type(Height))
